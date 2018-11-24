fef54239b081fad865244ee21d106544b8c1c9bc
Rousonur Jaman <rony@cefalo.com>
2018-11-24 16:14:50 +0600

    added enums

---
+++ Repository.Models/Enums/CardTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardValidation.Repository.Models.Enums
+{
+    public enum CardTypeEnum
+    {
+        Visa = 1,
+        Master = 2,
+        Amex = 3,
+        JCB = 4,
+        Unknown = 5
+    }
+}
---
+++ Repository.Models/Enums/ResultEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardValidation.Repository.Models.Enums
+{
+    public enum ResultEnum
+    {
+        Valid = 1,
+        Invalid = 2,
+        //[StringValue("Does not exist")]
+        DoesNotExist = 3,
+    }
+}
